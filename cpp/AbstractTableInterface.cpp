37460bc77f2938e8d8589e7a624cff766f33bd35
Nathaniel <nathaniel@followmyvote.com>
2024-10-07 16:55:44 -0500

    Give Up GitHub

--- cpp/AbstractTableInterface.cpp
+++
@@ -1,6 +0,0 @@
-#include <AbstractTableInterface.hpp>
-#include <BlockchainInterface.hpp>
-
-// We can't inline this because we need to see the real definition of BlockchainInterface to cast it to QObject
-AbstractTableInterface::AbstractTableInterface(BlockchainInterface* blockchain, QString scope)
-    : QObject(blockchain), blockchain(blockchain), scope(scope) {}
--- qml/BigX.qml
+++
@@ -1,9 +0,0 @@
-import QtQuick 2.14
-
-Rectangle {
-    id: bigX
-    color: "black"
-    layer.enabled: true
-    Rectangle { anchors.centerIn: parent; width: parent.width / 4; height: 10; rotation: 20 }
-    Rectangle { anchors.centerIn: parent; width: parent.width / 4; height: 10; rotation: -20 }
-}
